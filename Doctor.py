7be30c7ad310581c4e42dd2a2664446dad3bcc6e
Jesus Santana <jscheveto7@gmail.com>
2023-06-23 05:14:31 +0000

    Code to get first doctor

--- Doctor.py
+++ Doctor.py
@@ -1,7 +1,13 @@
 import requests
 import json
 
-url = 'https://api.catalogopolis.xyz/v1/actors'
+url = 'https://api.catalogopolis.xyz/v1/doctors'
 response = requests.get(url)
-print(response.json())
-
+# print(response.json())
+# print(response.json()[0].get('id'))
+url2 = 'https://api.catalogopolis.xyz/v1/doctors/'+str(response.json()[0].get('id'))+'/actors'
+response2 = requests.get(url2)
+# print(response2.json())
+# print(url2)
+print(response.json()[0].get('incarnation'))
+print(response2.json()[0].get('name'))
